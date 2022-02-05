f69608efed2ef6101dde1f23ef30d214533d5cb2
Kobe Ruado <insidiae423@gmail.com>
2022-02-05 19:14:51 +0800

    Finish Exercise 2

---
+++ 03-advanced-react-hooks/exercises/02.extra-3.js
@@ -0,0 +1,159 @@
+// useCallback: custom hooks
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from "react";
+import {
+  fetchPokemon,
+  PokemonForm,
+  PokemonDataView,
+  PokemonInfoFallback,
+  PokemonErrorBoundary,
+} from "../pokemon";
+
+// 💯 See if you can make a reusable hook that only
+// calls `dispatch` if the component is mounted.
+function useSafeDispatch(unsafeDispatch) {
+  const mountedRef = React.useRef(false);
+
+  // to make this even more generic you should use the useLayoutEffect hook to
+  // make sure that you are correctly setting the mountedRef.current immediately
+  // after React updates the DOM. Even though this effect does not interact
+  // with the dom another side effect inside a useLayoutEffect which does
+  // interact with the dom may depend on the value being set
+  React.useLayoutEffect(() => {
+    mountedRef.current = true;
+
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+
+  return React.useCallback(
+    (...args) => {
+      if (mountedRef.current) {
+        unsafeDispatch(...args);
+      }
+    },
+    [unsafeDispatch]
+  );
+}
+
+function asyncReducer(state, action) {
+  switch (action.type) {
+    case "pending": {
+      return { status: "pending", data: null, error: null };
+    }
+    case "resolved": {
+      return { status: "resolved", data: action.data, error: null };
+    }
+    case "rejected": {
+      return { status: "rejected", data: null, error: action.error };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
+function useAsync(initialState) {
+  const [state, unsafeDispatch] = React.useReducer(asyncReducer, {
+    status: "idle",
+    data: null,
+    error: null,
+    ...initialState,
+  });
+
+  // 💯 Prevent `dispatch` from being called if the component is unmounted.
+  const dispatch = useSafeDispatch(unsafeDispatch);
+
+  const run = React.useCallback(
+    (promise) => {
+      dispatch({ type: "pending" });
+      promise.then(
+        (data) => {
+          dispatch({ type: "resolved", data });
+        },
+        (error) => {
+          dispatch({ type: "rejected", error });
+        }
+      );
+    },
+    [dispatch]
+  );
+
+  return { ...state, run };
+}
+
+function PokemonInfo({ pokemonName }) {
+  const {
+    data: pokemon,
+    status,
+    error,
+    run,
+  } = useAsync({ status: pokemonName ? "pending" : "idle" });
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return;
+    }
+    const pokemonPromise = fetchPokemon(pokemonName);
+    run(pokemonPromise);
+  }, [pokemonName, run]);
+
+  switch (status) {
+    case "idle":
+      return <span>Submit a pokemon</span>;
+    case "pending":
+      return <PokemonInfoFallback name={pokemonName} />;
+    case "rejected":
+      throw error;
+    case "resolved":
+      return <PokemonDataView pokemon={pokemon} />;
+    default:
+      throw new Error("This should be impossible");
+  }
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState("");
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName);
+  }
+
+  function handleReset() {
+    setPokemonName("");
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonErrorBoundary onReset={handleReset} resetKeys={[pokemonName]}>
+          <PokemonInfo pokemonName={pokemonName} />
+        </PokemonErrorBoundary>
+      </div>
+    </div>
+  );
+}
+
+function AppWithUnmountCheckbox() {
+  const [mountApp, setMountApp] = React.useState(true);
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={mountApp}
+          onChange={(e) => setMountApp(e.target.checked)}
+        />{" "}
+        Mount Component
+      </label>
+      <hr />
+      {mountApp ? <App /> : null}
+    </div>
+  );
+}
+
+export default AppWithUnmountCheckbox;
--- 03-advanced-react-hooks/readme.md
+++ 03-advanced-react-hooks/readme.md
@@ -12,3 +12,17 @@
   4. [Traditional dispatch object with a type and switch statement](exercises/01.extra-4.js)
 
 We are introduced to the `useReducer` hook, which is an alternative of sorts to `useState` that has an API that is easily recognizable by someone familiar with Redux. With `useReducer`, we can still do exactly the same thing we do with `useState` (and even simulate how the old `this.setState` API works for class-based components), though one major advantage of `useReducer` is that it is more optimized to handle complex state objects (like how we merged multiple state variables into a single object in the previous workshop) by using the `dispatch` method to change specific sub-values depending on the `action.type`. We can also easily implement lazy initialization (again, like we did in the previous workshop) by passing the initializer function as a third argument to `useReducer`.
+
+### 2. useCallback: custom hooks
+
+- [Exercise Solution](exercises/02.js)
+- 💯 Extra Credit
+  1. [Use useCallback to empower the user to customize memoization](exercises/02.extra-1.js)
+  2. [Return a memoized `run` function from useAsync](exercises/02.extra-2.js)
+  3. [Make safeDispatch with useCallback, useRef, and useEffect](exercises/02.extra-3.js)
+
+We are introduced to the `useCallback` hook, which is a mechanism for memoizing expensive or otherwise time-consuming functions (i.e. async callbacks). We also get more practice writing custom hooks to extract the commonly used logic for our async calls into a reusable `useAsync` hook.
+
+Upon first writing the `useAsync` hook, we encounter the `exhaustive-deps` ESLint warning and we immediately see one of the use-cases for `useCallback` as it enables us to move the concerns of managing the dependency list into a more proper place and better help ESLint catch potential errors. We also improve the API for our custom `useAsync` hook by returning a memoized `run` function which can then be used by the users of this API on their own `useEffect`.
+
+Finally, we also solve a potential bug caused by the component being unmounted in the middle of an async call, resulting in the `dispatch` function being called for an already unmounted component. To solve this, we make yet another custom hook called `useSafeDispatch` that simply checks whether the component is still mounted before actually running the `dispatch` function.
