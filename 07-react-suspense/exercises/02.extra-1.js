69e69e7884d9c09b3c5974c40c2c821311862e8b
Kobe Ruado <insidiae423@gmail.com>
2022-02-20 00:01:37 +0800

    Finish Exercise 2

---
+++ 07-react-suspense/exercises/02.extra-1.js
@@ -0,0 +1,80 @@
+// Render as you fetch
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from "react";
+import {
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonForm,
+  PokemonDataView,
+  PokemonErrorBoundary,
+} from "../pokemon";
+import { createResource } from "../utils";
+
+function PokemonInfo({ pokemonResource }) {
+  const pokemon = pokemonResource.read();
+
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  );
+}
+
+function createPokemonResource(pokemonName) {
+  return createResource(fetchPokemon(pokemonName));
+}
+function App() {
+  const [pokemonName, setPokemonName] = React.useState("");
+  const [pokemonResource, setPokemonResource] = React.useState(null);
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      setPokemonResource(null);
+    } else {
+      setPokemonResource(createPokemonResource(pokemonName));
+    }
+  }, [pokemonName]);
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName);
+  }
+
+  function handleReset() {
+    setPokemonName("");
+  }
+
+  // 💯 Try to play around with wrapping more of your elements in these boundaries
+  // and see what changes with the user experience.
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <React.Suspense
+        fallback={
+          <div className="pokemon-info">
+            <PokemonInfoFallback name={pokemonName} />
+          </div>
+        }
+      >
+        <div className="pokemon-info">
+          {pokemonResource ? (
+            <PokemonErrorBoundary
+              onReset={handleReset}
+              resetKeys={[pokemonName]}
+            >
+              <PokemonInfo pokemonResource={pokemonResource} />
+            </PokemonErrorBoundary>
+          ) : (
+            "Submit a pokemon"
+          )}
+        </div>
+      </React.Suspense>
+    </div>
+  );
+}
+
+export default App;
--- 07-react-suspense/readme.md
+++ 07-react-suspense/readme.md
@@ -13,3 +13,15 @@
 We dive into the basics of the experimental `React.Suspense` API, letting us define a promise ahead of time, throwing the promise while it fetches its data, then using `React.Suspense` to rerender the component when the promise gets resolved. We can also use the `ErrorBoundary` component to catch errors if the promise gets rejected for any reason.
 
 We can then genericize this logic to make a reusable utility function that does the data fetching and error handling work for us, and with the `React.Suspense` API, this leaves the components that use this utility function fully declarative.
+
+### 2. Render as you fetch
+
+- [Exercise Solution](exercises/02.js)
+- 💯 Extra Credit
+  1. [Suspense and Error Boundary positioning](exercises/02.extra-1.js)
+
+The examples demonstrate three different options for fetching+rendering data: The first option is called "Render on fetch", where you render the structure of the component first before making a fetch call to get the data that you actually need to display. The second option is called "Fetch then render", where you make the fetch for the data you need before rendering the (lazy-loaded) component. These first two options share a similar downside: You have to make two separate requests, and the first request has to finish _before_ you even start the other request, which can lead to an ugly waterfall of requests where some requests depend on the completion of other requests, and lead to a non-optimal experience especially for users with slow network speeds.
+
+The "Render as you fetch" method discussed in this exercise solves exactly this problem, since you can make the request for the code _and_ make a request for the data at the same time.
+
+In this exercise, we make some refactors to our code that leverages React's experimental Suspense API to massively clean up the components that render the fetched data.
